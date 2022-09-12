41a61c7733405e1a91dcd4afc3a18106f39481da
akash gaba <aakashgaba@gmail.com>
2022-09-12 23:49:14 +0530

    some changes (#3)
    Co-authored-by: Akash Gaba <akashgaba@LAPTOP-CE0MTMGM.localdomain>

---
+++ js/app.js
@@ -0,0 +1,12 @@
+import myJson from '../data/data.json' assert {type: 'json'};
+let app = Vue.createApp({
+    data: function () {
+        return {
+            name: myJson.name,
+            profession: myJson.profession,
+            emails: myJson.emails.join(' ,'),
+            phone_numbers: myJson.phone_numbers.join(' ,')
+        }
+    }
+});
+app.mount('#app');
