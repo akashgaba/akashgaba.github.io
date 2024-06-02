93bd04176e27b22485920cb8e304e0c9e0a88f8a
Akash Gaba <akashgaba.work@gmail.com>
2024-06-02 17:25:30 +0530

    fix

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 import myJson from '../data/data.json' assert {type: 'json'};
 let app = Vue.createApp({
     data: function () {
+        let transformedSkills = myJson.skills.map(skill => ({
+            name: skill.name,
+            items: skill.items.join(', ')
+        }));
         return {
             name: myJson.name,
             profession: myJson.profession,
             emails: myJson.emails.join(', '),
             phone_numbers: myJson.phone_numbers.join(', '),
             work_experiences : myJson.work_experiences,
-            education_experiences : myJson.education_experiences
+            education_experiences : myJson.education_experiences,
+            skills : transformedSkills
         }
     }
 });
